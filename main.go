99f4d426362e767748b0ea3fdb3cd196a2af82da
Karn Wong <karn@karnwong.me>
2025-07-24 16:55:13 +0700

    init cobra

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/kahnwong/habit-tracker/calendar"
-)
+import "github.com/kahnwong/habit-tracker/cmd"
 
 func main() {
-	// Define dates to highlight (example: today, tomorrow, and a date in the past)
-	highlightDates := []time.Time{
-		time.Date(2025, time.June, 15, 0, 0, 0, 0, time.Local),
-		time.Date(2025, time.August, 20, 0, 0, 0, 0, time.Local),
-		time.Date(2025, time.May, 10, 0, 0, 0, 0, time.Local),
-		time.Date(2025, time.July, 10, 0, 0, 0, 0, time.Local),
-	}
-
-	calendar.RenderCalendarView(3, highlightDates)
-
-	fmt.Printf("----%s\n", time.Now()) // debug
+	cmd.Execute()
 }
