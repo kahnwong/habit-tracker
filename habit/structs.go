630ef2c3d07c1c5b1a5d667c3ec8397e6b968c09
Karn Wong <karn@karnwong.me>
2025-09-10 14:57:55 +0700

    refactor: use sqlite-base

--- habit/structs.go
+++
@@ -1,7 +0,0 @@
-package habit
-
-type Activity struct {
-	Date        string `db:"date"`
-	IsCompleted int    `db:"is_completed"` // 0 for false, 1 for true
-	HabitName   string `db:"habit_name"`
-}
