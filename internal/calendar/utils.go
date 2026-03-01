e64bd17fb91f041a0c52ed4a2b1694eec76f25dd
Karn Wong <karn@karnwong.me>
2026-03-01 14:12:29 +0700

    fix: remove previous year filter

--- internal/calendar/utils.go
+++ internal/calendar/utils.go
@@ -11,19 +11,7 @@ func generateMonths(lookbackMonths int) []time.Time {
 		allMonths[lookbackMonths-1-i] = now.AddDate(0, -i, 0)
 	}
 
-	return removePreviousYearDates(allMonths)
-}
-
-func removePreviousYearDates(dates []time.Time) []time.Time {
-	var currentYearDates []time.Time
-	currentYear := now.Year()
-
-	for _, date := range dates {
-		if date.Year() == currentYear {
-			currentYearDates = append(currentYearDates, date)
-		}
-	}
-	return currentYearDates
+	return allMonths
 }
 
 func getMaxWeeks(currentBlockMonths []time.Time) int {
